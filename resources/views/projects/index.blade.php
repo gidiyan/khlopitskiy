78802425dd265bc5ae240b8f19f12120d9a070c1
gidiyan <70430920+gidiyan@users.noreply.github.com>
2021-01-13 12:23:11 +0200

    testing admin

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,6 +1,6 @@
 <x-site-layout>
 <x-site-navigation></x-site-navigation>
-    <x-site-slider></x-site-slider>
+{{--    <x-site-slider></x-site-slider>--}}
 <div class="py-6 text-center bg-color3 color4">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-6">
         <div class="bg-color2 py-6 mt-2 mb-2">
