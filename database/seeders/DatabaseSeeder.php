a161a9ac11be958b995a32a879141d9a3af51dba
gidiyan <70430920+gidiyan@users.noreply.github.com>
2021-01-13 11:18:14 +0200

    testing admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\DB::statement("TRUNCATE TABLE products");
+        \DB::statement("TRUNCATE TABLE category_product");
+        \DB::statement("TRUNCATE TABLE products");
 //         \App\Models\User::factory(10)->create();
         \App\Models\Brand::factory(10)->create();
         \App\Models\Product::factory(10)->create();
